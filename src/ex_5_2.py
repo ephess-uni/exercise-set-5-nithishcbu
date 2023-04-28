f57b281989608f6c78784e17b529c978969941c5
nithishcbu <128633956+nithishcbu@users.noreply.github.com>
2023-04-29 03:54:26 +0530

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -20,11 +20,19 @@ if __name__ == "__main__":
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
     data = np.loadtxt(INFILE, delimiter=',')
-    data -= np.mean(data)
-    data /= np.std(data)
-    processed = data
-    # Save the output to OUTFILE using numpy routines.
-    np.savetxt(OUTFILE, processed, delimiter=',')
+#     data -= np.mean(data)
+#     data /= np.std(data)
+#     processed = data
+#     # Save the output to OUTFILE using numpy routines.
+#     np.savetxt(OUTFILE, processed, delimiter=',')
+
+    centered = data - np.mean(data, axis=0)
+
+    # TODO: scale the centered data by dividing each column by its standard deviation
+    scaled = centered / np.std(centered, axis=0)
+
+    # TODO: save the processed data to a file
+    np.savetxt(OUTFILE, scaled)
 
 
    
