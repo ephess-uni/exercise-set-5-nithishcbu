cc8cd53b900c90dc583436861ec6585090a6e4cd
nithishcbu <128633956+nithishcbu@users.noreply.github.com>
2023-04-28 20:36:49 +0530

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -15,12 +15,26 @@ except ImportError:
 
 
 if __name__ == "__main__":
-
     # Use these predefined input / output files
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
+    
 
     # Complete the data processing steps using numpy here.
+    # load data from file
+    data = np.loadtxt(INFILE)
+
+    # calculate mean and standard deviation
+    mean = np.mean(data)
+    std = np.std(data)
+
+    # subtract mean and divide by standard deviation
+    zero_mean = (data - mean) / std
+    
+    # save processed data to a new file
+    processed = zero_mean
+    np.savetxt(OUTFILE, processed)
 
     # Save the output to OUTFILE using numpy routines.
+   
