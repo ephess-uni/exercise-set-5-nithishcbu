7d5d41241046ac319adecc60f3e735e2d75e6faf
nithishcbu <128633956+nithishcbu@users.noreply.github.com>
2023-04-29 03:48:36 +0530

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -19,11 +19,13 @@ if __name__ == "__main__":
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
-    l1 = np.loadtxt(INFILE)
-    l1=l1-l1.mean()
-    y=l1.std()
-    processed=l1/y
-    np.savetxt(OUTFILE, processed, fmt='%.2e')
-        
+    data = np.loadtxt(INFILE, delimiter=',')
+    data -= np.mean(data)
+    data /= np.std(data)
+    processed = data
+
     # Save the output to OUTFILE using numpy routines.
+    np.savetxt(OUTFILE, processed, delimiter=',')
+
+
    
